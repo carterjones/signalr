9204aeec0072eb8271ed4af85d57bf8a03cd8c07
Carter Jones <carter@carterjones.info>
2017-12-30 13:28:41 -0800

    add more thorough tests for negotiate

--- signalr_test.go
+++ signalr_test.go
@@ -10,6 +10,7 @@ import (
 	"runtime"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/carterjones/helpers/trace"
 	"github.com/carterjones/signalr"
@@ -30,6 +31,43 @@ func notNil(tb testing.TB, id string, act interface{}) {
 		tb.Errorf("\n\033[31m%s:%d (%s):\n\n\texp: a non-nil value\n\tgot: %#v\033[39m\n\n",
 			filepath.Base(file), line, id, act)
 	}
+}
+
+// Note: this is largely derived from
+// https://github.com/golang/go/blob/1c69384da4fb4a1323e011941c101189247fea67/src/net/http/response_test.go#L915-L940
+func errMatches(tb testing.TB, id string, err error, wantErr interface{}) {
+	if err == nil {
+		if wantErr == nil {
+			return
+		}
+
+		if sub, ok := wantErr.(string); ok {
+			tb.Errorf("%s: unexpected success; want error with substring %q", id, sub)
+			return
+		}
+
+		tb.Errorf("%s: unexpected success; want error %v", id, wantErr)
+		return
+	}
+
+	if wantErr == nil {
+		tb.Errorf("%s: %v; want success", id, err)
+		return
+	}
+
+	if sub, ok := wantErr.(string); ok {
+		if strings.Contains(err.Error(), sub) {
+			return
+		}
+		tb.Errorf("%s, error = %v; want an error with substring %q", id, err, sub)
+		return
+	}
+
+	if err == wantErr {
+		return
+	}
+
+	tb.Errorf("%s: %v; want %v", id, err, wantErr)
 }
 
 func hostFromServerURL(url string) (host string) {
@@ -64,13 +102,18 @@ func newTestClient(protocol, endpoint, connectionData string, ts *httptest.Serve
 	c.HTTPClient = ts.Client()
 
 	// Save the TLS config in case this is using TLS.
-	c.TLSClientConfig = ts.TLS
+	if ts.TLS != nil {
+		c.TLSClientConfig = ts.TLS
+		c.Scheme = signalr.HTTPS
+	} else {
+		c.Scheme = signalr.HTTP
+	}
 
 	return
 }
 
 func negotiate(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte(`{"ConnectionToken":"hello world","ConnectionId":"1234-ABC","URL":"/signalr"}`))
+	_, err := w.Write([]byte(`{"ConnectionToken":"hello world","ConnectionId":"1234-ABC","URL":"/signalr","ProtocolVersion":"1337"}`))
 	if err != nil {
 		trace.Error(err)
 		return
@@ -114,11 +157,107 @@ func start(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte(`{"Response":"started"}`))
 	if err != nil {
 		trace.Error(err)
-		return
 	}
 }
 
+func throw503Error(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusServiceUnavailable)
+	w.Write([]byte("503 error"))
+}
+
+func throw123Error(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(123)
+	w.Write([]byte("123 error"))
+}
+
+func throwMalformedStatusCodeError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(9001)
+	w.Write([]byte("malformed status code"))
+}
+
 func TestClient_Negotiate(t *testing.T) {
+	cases := map[string]struct {
+		fn      http.HandlerFunc
+		in      signalr.Client
+		TLS     bool
+		exp     signalr.Client
+		wantErr string
+	}{
+		"successful http": {
+			fn: negotiate,
+			in: signalr.Client{
+				Protocol:       "1337",
+				Endpoint:       "/signalr",
+				ConnectionData: "all the data",
+			},
+			TLS: false,
+			exp: signalr.Client{
+				Protocol:        "1337",
+				Endpoint:        "/signalr",
+				ConnectionToken: "hello+world",
+				ConnectionID:    "1234-ABC",
+			},
+		},
+		"successful https": {
+			fn: negotiate,
+			in: signalr.Client{
+				Protocol:       "1337",
+				Endpoint:       "/signalr",
+				ConnectionData: "all the data",
+			},
+			TLS: true,
+			exp: signalr.Client{
+				Protocol:        "1337",
+				Endpoint:        "/signalr",
+				ConnectionToken: "hello+world",
+				ConnectionID:    "1234-ABC",
+			},
+		},
+		"503 error": {
+			fn:      throw503Error,
+			wantErr: "503 Service Unavailable",
+		},
+		"default error": {
+			fn:      throw123Error,
+			wantErr: "123 status code",
+		},
+		"failed get request": {
+			fn:      throwMalformedStatusCodeError,
+			wantErr: "malformed HTTP status code",
+		},
+		"invalid json": {
+			fn: func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("invalid json"))
+			},
+			wantErr: "invalid character 'i' looking for beginning of value",
+		},
+	}
+
+	for id, tc := range cases {
+		// Create a test server.
+		ts := newTestServer(http.HandlerFunc(tc.fn), tc.TLS)
+		defer ts.Close()
+
+		// Create a test client.
+		c := newTestClient(tc.in.Protocol, tc.in.Endpoint, tc.in.ConnectionData, ts)
+
+		// Set the wait time to milliseconds.
+		c.RetryWaitDuration = 1 * time.Millisecond
+
+		// Perform the negotation.
+		err := c.Negotiate()
+
+		// Make sure the error matches the expected error.
+		if tc.wantErr != "" {
+			errMatches(t, id, err, tc.wantErr)
+		}
+
+		// Validate the things we expect.
+		equals(t, id, tc.exp.ConnectionToken, c.ConnectionToken)
+		equals(t, id, tc.exp.ConnectionID, c.ConnectionID)
+		equals(t, id, tc.exp.Protocol, c.Protocol)
+		equals(t, id, tc.exp.Endpoint, c.Endpoint)
+	}
 }
 
 func TestClient_Connect(t *testing.T) {
